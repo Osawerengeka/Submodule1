22598faa00a2b6da2dbc9d845804cda618183504
Osawerengeka <mr.georgegevork@gmail.com>
2020-03-18 20:33:52 +0300

    Добавьте файлы проекта.

---
+++ Submodule1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Submodule1
+{
+    public class Security
+    {
+        public int hashfunc(string pass)
+        {
+            int hash = 0;
+            for (int i = 1; i < pass.Length; i++)
+            {
+                hash = hash + pass[i] * pass[0];
+
+            }
+            return hash;
+        }
+    }
+}
